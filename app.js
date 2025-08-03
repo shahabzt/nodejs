d45e17b5719de83948ff2dd293d9e81fe3057104
sh.banijamali <sh.banijamali@podro.com>
2025-08-03 14:17:58 +0330

    feat: implement a new middleware and logger in codes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use(helmet())
-app.use(morgan("tiny"))
+if(app.get("env") === "development") app.use(morgan("tiny"))
 
 app.use(logger)
 app.use(auth)
